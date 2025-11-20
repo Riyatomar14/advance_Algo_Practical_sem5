59b814db2c1f7dfc85fddfd56d492f0ee1472f4a
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2025-11-20 16:41:14 +0530

    Update Kruskal's Algorithm.cpp

--- Kruskal's Algorithm.cpp
+++ Kruskal's Algorithm.cpp
@@ -2,16 +2,19 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-vector<int> parent, sz;  
+
+vector<int> parent, sz;
 
 void make_set(int v) {
     parent[v] = v;
     sz[v] = 1;
 }
+
 int find_set(int v) {
     if (v == parent[v]) return v;
     return parent[v] = find_set(parent[v]);
 }
+
 void union_sets(int a, int b) {
     a = find_set(a);
     b = find_set(b);
@@ -19,11 +22,13 @@ void union_sets(int a, int b) {
         if (sz[a] < sz[b]) swap(a, b);
         parent[b] = a;
         sz[a] += sz[b];
-        
     }
 }
+
 int main() {
     int n = 4, m = 5;
+
+    // weight, (u, v)
     vector< pair<int, pair<int, int>> > edges = {
         {1, {1, 2}},
         {4, {1, 3}},
@@ -37,9 +42,14 @@ int main() {
 
     for (int i = 1; i <= n; ++i)
         make_set(i);
+
     sort(edges.begin(), edges.end());
+
     vector<pair<int,int>> mst;
     int total_cost = 0;
+
+    // ⭐ Your missing loop (fixed)
+    for (auto &edge : edges) {
         int w = edge.first;
         int u = edge.second.first;
         int v = edge.second.second;
@@ -50,6 +60,7 @@ int main() {
             total_cost += w;
         }
     }
+
     cout << "Edges in MST:\n";
     for (auto &e : mst)
         cout << e.first << " - " << e.second << "\n";
@@ -58,3 +69,4 @@ int main() {
 
     return 0;
 }
+
